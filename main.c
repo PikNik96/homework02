c7edc9a8c4ea8bc6dc1f87fa27f0f75fae614df9
PikNik <numizma9@gmail.com>
2021-09-17 17:57:30 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <limits.h>
+
+int main(void) {
+	double x = 7.0;
+	int integer = (int)x;
+	double remain = x - integer;
+
+	printf("Remain = %lf\n", remain);
+
+	int leading_zeroes = -1;
+
+	while(remain < 1.0) {
+		leading_zeroes++;
+		remain *= 10;
+	}
+
+	printf("Leading zeroes = %d\n", leading_zeroes);
+	printf("Remain = %lf\n", remain);
+
+	while((long)remain * 10 < INT_MAX)
+		remain *= 10;
+
+	printf("Remain = %ld (max %d)\n", (long)remain, INT_MAX);
+	
+	while(!((long)remain % 10))
+		remain /= 10.;
+
+	printf("Remain = %ld\n", (long)remain);
+
+	int fract = (int)remain;
+
+	while (leading_zeroes--)
+		putchar('0');
+	printf("%d\n", fract);
+}
